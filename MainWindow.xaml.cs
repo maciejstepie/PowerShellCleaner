35ac4a73cd90bad73aac3a9d6c4ffc08b0c37539
theSpawacz <56389913+maciejstepie@users.noreply.github.com>
2022-03-07 00:36:26 +0100

    init

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,62 @@ namespace PowerShellCleaner
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = @"%userprofile%\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt";
+        string[] lines;
         public MainWindow()
         {
             InitializeComponent();
+            LoadCommandHistory();
+        }
+
+        private void LoadCommandHistory()
+        {
+            string systemPath = Environment.ExpandEnvironmentVariables(path);
+            lines = File.ReadAllLines(systemPath);
+
+            foreach (string line in lines)
+            {
+                HistoryCommandsList.Items.Add(line);
+            }
+           
+        }
+
+        private void RemoveDuplicateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Double reverse to keep recent ones 
+            lines = lines.Reverse().Distinct().Reverse().ToArray();
+
+            HistoryCommandsList.Items.Clear();
+
+            foreach (string line in lines)
+            {
+                HistoryCommandsList.Items.Add(line);
+            }
+        }
+
+        private void RemoveLineButton_Click(object sender, RoutedEventArgs e)
+        {
+            HistoryCommandsList.Items.Remove(((Button)sender).DataContext);
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string systemPath = Environment.ExpandEnvironmentVariables(path);
+            await File.WriteAllLinesAsync(systemPath, lines);
+        }
+
+        private void RealodButton_Click(object sender, RoutedEventArgs e)
+        {
+            string systemPath = Environment.ExpandEnvironmentVariables(path);
+            lines = File.ReadAllLines(systemPath);
+
+            HistoryCommandsList.Items.Clear();
+
+            foreach (string line in lines)
+            {
+                HistoryCommandsList.Items.Add(line);
+            }
         }
     }
 }
